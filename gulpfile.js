425e228060374057c0f6a9ccef99cfcdb8129a5e
Cronfy <cronfy@gmail.com>
2017-08-15 23:15:19 +0300

    gulp: lint-git и локальная конфигурация для него

--- gulpfile.js
+++ gulpfile.js
@@ -40,3 +40,58 @@ gulp.task('lint-html', function() {
 
 })
 
+/**
+ * Проверяет, что в проекте изменены только разрешенные к изменению файлы
+ */
+gulp.task('lint-git', function () {
+    var config = {
+        allow: [
+            /^web\/frontend\//,
+            /^apps\/main\/views\/layouts\/layout\.php$/,
+            /^apps\/main\/views\/template\/[a-z]+\.php$/,
+        ],
+        ignore: [],
+        sha: 'be3391e'
+    }
+
+    var fs = require('fs');
+
+    if (fs.existsSync('./lint-config-local.js')) {
+        local_config = require('./lint-config-local.js');
+        if (local_config['lint-git'].ignore) {
+            config.ignore = config.ignore.concat(local_config['lint-git'].ignore);
+        }
+    }
+
+
+    var shell = require('shelljs');
+
+    var result = shell.exec('git diff --name-only ' + config.sha + ' master@{0}', {silent:true});
+
+    if (result.code !== 0) {
+        throw new Error("Failed to run git");
+    }
+
+    var files = result.stdout;
+
+    files.split("\n").forEach(function (line) {
+        if (!line.trim()) return
+
+        var matched = false
+        config.allow.forEach(function (regex) {
+            if (line.match(regex)) {
+                matched = true;
+            }
+        })
+        config.ignore.forEach(function (str) {
+            if (line === str) {
+                matched = true;
+            }
+        })
+        if (!matched) {
+            throw new Error("Line does not match: " + line);
+        }
+    })
+})
+
+gulp.task('test', ['lint-git', 'lint-css', 'lint-html']);
