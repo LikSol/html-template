1479f32025a3ac9b0a1f0f2991c68c89682610ee
Cronfy <cronfy@gmail.com>
2017-08-16 23:56:44 +0300

    stylelint-media-query-order: более понятные отчеты

--- stylelint/media-query-order/index.js
+++ stylelint/media-query-order/index.js
@@ -5,37 +5,65 @@ var stylelint = require("stylelint")
 
 const _ = require("lodash");
 const mediaParser = require("postcss-media-query-parser");
+const report = require("stylelint/lib/utils/report");
 
 var ruleName = "plugin/media-query-order"
 const messages = stylelint.utils.ruleMessages(ruleName, {
-    expected: selectorValue =>
-        `Expected class selector "${selectorValue}" to match specified pattern`
+    expectedAfter: selectorValue =>
+        `Expected atrule "${selectorValue}" to be positioned after regular rules`,
+    expectedPattern: (selectorValue, pattern) =>
+        `Expected media rule "${selectorValue}" to match pattern "${pattern}"`,
+    expectedOrder: (current, previous) =>
+        `Expected media rule "${current}" to be positioned before "${previous}" (in ascending order)`
 });
 
 module.exports = stylelint.createPlugin(ruleName, function(pattern, options) {
     return function(root, result) {
 
-        var atRulesStarted = false;
-        root.each(function (node) {
-            if (atRulesStarted) {
+        let atrule
+        root.some(function (node) {
+            if (atrule) {
                 if (node.type !== 'comment' && node.type !== 'atrule') {
-                    throw "Media query must go after other rules" + node.toString()
+                    report({
+                        result,
+                        ruleName,
+                        message: messages.expectedAfter('@' + atrule.name + ' ' + atrule.params),
+                        node: atrule,
+                        index: atrule.sourceIndex
+                    });
+                    return true
                 }
             }
 
-            if (node.type === 'atrule') {
-                atRulesStarted = true
+            if (!atrule && node.type === 'atrule' && node.name !== 'import') {
+                atrule = node
             }
         })
 
-        var lastPixels = 0;
-        root.walkAtRules(rule => {
+        let lastPixels = 0
+        let previous
 
+        root.walkAtRules(rule => {
+            const current_pattern = mediaToString(mediaParser.default(rule.params));
+            const allowed_pattern = mediaToString(mediaParser.default('screen and (min-width: 100px)'));
+
+            if (current_pattern !== allowed_pattern) {
+                // пока поддерживаем только простые варианты:
+                // screen and (min-width: ...)
+                return
+            }
             var pixels = check(rule);
             if (pixels <= lastPixels) {
-                throw "Each next media query must describe larger width than previous: " + pixels
+                report({
+                    result,
+                    ruleName,
+                    message: messages.expectedOrder(rule.params.toString(), previous.params.toString()),
+                    node: rule,
+                    index: rule.sourceIndex
+                });
             }
 
+            previous = rule
             lastPixels = pixels
         });
 
@@ -48,7 +76,7 @@ module.exports = stylelint.createPlugin(ruleName, function(pattern, options) {
                         if (!/^[0-9]+px/.test(nodeValue.value)) {
                             throw "Unknown value " + nodeValue.value;
                         }
-                        resultArray.push(0);
+                        resultArray.push('...');
                     } else {
                         resultArray.push(nodeValue.value)
                     }
@@ -75,13 +103,22 @@ module.exports = stylelint.createPlugin(ruleName, function(pattern, options) {
         }
 
         function check(rule) {
-            const result = mediaParser.default(rule.params);
+            const current = mediaParser.default(rule.params);
+            const current_string = mediaToString(current)
             const pattern = mediaParser.default('screen and (min-width: 100px)');
+            const pattern_string = mediaToString(pattern)
 
-            if (mediaToString(result) !== mediaToString(pattern)) {
-                throw "Media query does not match pattern " + rule.params;
+            if (current_string !== pattern_string) {
+                report({
+                    result,
+                    ruleName,
+                    message: messages.expectedPattern(current_string, pattern_string),
+                    node: rule,
+                    index: rule.sourceIndex
+                });
             }
-            return getMediaPixels(result);
+
+            return getMediaPixels(current);
         }
 
     }
