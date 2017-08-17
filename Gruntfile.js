cc082c2e434740d72e8c776395138a716f21d189
Cronfy <cronfy@gmail.com>
2017-08-17 18:03:06 +0300

    grunt: htmlhint-plus: возможность указать имя файла для валидации

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,37 @@
+"use strict"
+
 /*
 gulp lint-css
 grunt htmlhintplus
 gulp lint-html
  */
 
+
 module.exports = function(grunt) {
 
-  grunt.loadNpmTasks('grunt-htmlhint-plus');
+    grunt.loadNpmTasks('grunt-htmlhint-plus');
 
-    grunt.initConfig({
-        htmlhintplus: {
-            build: {
-                options: {
-                    force: true,
-                    extendRules: true,
-                    rules: {
-                        // builtin
-                        'id-class-value': false,
-                        'href-abs-or-rel': false,
+    grunt.task.registerTask('lint-html', 'description', function (file) {
+        grunt.initConfig({
+            htmlhintplus: {
+                build: {
+                    options: {
+                        force: true,
+                        extendRules: true,
+                        rules: {
+                            // builtin
+                            'id-class-value': false,
+                            'href-abs-or-rel': false,
+                        },
+                        customRules: [
+                        ],
                     },
-                    customRules: [
-                    ],
-                },
-                src: 'html/index.html'
+                    src: file
+                }
             }
-        }
-    });
+        })
+
+        grunt.task.run('htmlhintplus')
+    })
 }
+
